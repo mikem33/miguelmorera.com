acd1aae02d41fa07a407abf8185d80f4c07df5ec
Miguel Morera <info@miguelmorera.com>
2026-05-17 01:31:05 +0200

    fix: 404 form layout. Add form functionality to he search form

--- source/assets/javascript/source/form.js
+++ source/assets/javascript/source/form.js
@@ -1,13 +1,15 @@
 jQuery(document).ready(function($) {
+    var fieldSelector = 'input[type=text], input[type=search], input[type=email], input[type=tel], input[type=url], textarea';
+
     // Delete placeholder of the search form on focus.
-    $('input[type=text], input[type=email], input[type=tel], input[type=url], textarea').focus(function(){
+    $(document).on('focusin', fieldSelector, function(){
         $(this).data('placeholder',$(this).attr('placeholder'));
         $(this).attr('placeholder','');
         $(this).closest('fieldset').addClass('active');
     });
 
     // Turn on the placeholder when the field lose focus and there is no added content.
-    $('input[type=text], input[type=email], input[type=tel], input[type=url], textarea').blur(function(){
+    $(document).on('focusout', fieldSelector, function(){
         if (!$(this).val()) {
             $(this).closest('fieldset').removeClass('active');
             $(this).attr('placeholder',$(this).data('placeholder'));
--- source/searchform.php
+++ source/searchform.php
@@ -1,8 +1,10 @@
-<form method="get" id="searchform" class="flex" action="<?php bloginfo('url'); ?>/">
-    <input type="text" id="s" name="s" placeholder="<?php _e('Introduce tu búsqueda','prometheus'); ?>" value="<?php the_search_query(); ?>">
+<form method="get" id="searchform" class="flex mm__form" action="<?php bloginfo('url'); ?>/">
+    <fieldset class="fieldset fieldset--text">
+        <label for="s" class="label"><?php _e('Introduce tu búsqueda','prometheus'); ?></label>
+        <input type="text" id="s" name="s" placeholder="<?php _e('Introduce tu búsqueda','prometheus'); ?>" value="<?php the_search_query(); ?>">
+    </fieldset>
     <button class="button button--white-blue button--filled button--icon button--search" type="submit">
         <span><?php _e('Buscar', 'prometheus'); ?></span>
-        <svg width="15" height="15" class="ico"><use xlink:href="#ico-circle-arrow" /></svg
+        <svg width="15" height="15" class="ico"><use xlink:href="#ico-circle-arrow" /></svg>
     </button>
-    
 </form>
